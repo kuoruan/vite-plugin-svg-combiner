e63de2ec7d2276f1d395297ecc839c9d44bc5c85
Xingwang Liao <kuoruan@gmail.com>
2023-12-29 15:35:00 +0800

    feat: add support for ssr

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,27 +1,34 @@
-const root = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-
-root.id = "svg-sprite";
-
-root.style.position = "absolute";
-root.style.left = "-1px";
-root.style.top = "-1px";
-root.style.width = "0px";
-root.style.height = "0px";
+let root: SVGSVGElement;
 
 function insertSvgSprite() {
+  if (!root) return;
   document.body.insertBefore(root, document.body.firstChild);
 }
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", insertSvgSprite);
-} else {
-  insertSvgSprite();
+if (typeof document !== "undefined") {
+  root = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  root.id = "svg-sprite";
+
+  root.style.position = "absolute";
+  root.style.left = "-1px";
+  root.style.top = "-1px";
+  root.style.width = "0px";
+  root.style.height = "0px";
+  root.style.visibility = "hidden";
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", insertSvgSprite);
+  } else {
+    insertSvgSprite();
+  }
 }
 
 export function setRootId(id: string): void {
+  if (!root) return;
   root.id = id;
 }
 
 export function addSymbol(symbol: string): void {
-  root.insertAdjacentHTML("beforeend", symbol);
+  root?.insertAdjacentHTML("beforeend", symbol);
 }
